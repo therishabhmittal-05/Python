4a8c908981e7396f15fb686a7655df3bc236944e
therishabhmittal05 <rishabhmittal928@gmail.com>
2024-04-20 09:24:31 +0530

    Python

---
+++ calc.py
@@ -0,0 +1,5 @@
+print ("Write inputs: ")
+x = float(input ())
+y = float(input())
+print(f"{round(x+ y):,}" )
+
---
+++ python1.py
@@ -0,0 +1,10 @@
+# print("Hello, World!!")
+
+a = "Mittal" 
+b = 4 
+
+name = input("What's your name??\n").strip().title()
+
+first, last = name.split(" ")
+print("Name:", last )
+print (f"Hello, {first}")
